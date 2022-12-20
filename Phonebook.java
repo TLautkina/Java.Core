7909161eda0bdcd0aafa2f77ee6885249e823524
TLautkina <116433836+TLautkina@users.noreply.github.com>
2022-12-20 13:36:26 +0300

    Homework to lesson 4 is done

---
+++ Phonebook.java
@@ -0,0 +1,32 @@
+package ru.geekbrains.courses.javacore.LautkinaT.Lesson4.HW;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Phonebook {
+    private final Map<String, HashSet<String>> map;
+
+    Phonebook() {
+        this.map = new HashMap<>();
+    }
+
+    void add(String lastName, String phone) {
+        HashSet<String> numbers;
+
+        if (map.containsKey(lastName)) {
+            numbers = map.get(lastName);
+        } else {
+            numbers = new HashSet<>();
+        }
+        numbers.add(phone);
+        map.put(lastName, numbers);
+
+    }
+
+    Set<String> get(String lastName) {
+        return map.get(lastName);
+    }
+}
+
